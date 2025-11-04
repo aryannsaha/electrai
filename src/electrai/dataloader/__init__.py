54ed4fcd4b0433afc10c279e71795243437c269a
Hananeh Oliaei <ho0950@della-gpu.princeton.edu>
2025-11-04 13:39:16 -0500

    updated and cleaned src.electrai.dataloader

--- src/electrai/dataloader/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import importlib
-
-from .registry import DATASET_REGISTRY, get_dataset, register_dataset
-
-for module in ["chgcar_read"]:
-    importlib.import_module(f"{__name__}.{module}")
